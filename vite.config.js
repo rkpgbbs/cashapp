df81db803096d9754e03bcfedb764f8354d37dc1
rkpgbbs <rkp.gen@zoho.com>
2025-07-13 16:53:41 +0530

    misc

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/' // Changed from '/cashapp/' to '/' for AWS Amplify
+  base: '/', // Changed from '/cashapp/' to '/' for AWS Amplify
+  resolve: {
+    alias: {
+      './runtimeConfig': './runtimeConfig.browser',
+    },
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  }
 })
